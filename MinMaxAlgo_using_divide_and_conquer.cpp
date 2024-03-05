6c07da5089f440d85293663de56d4eb2c39d0573
Vrushali Sambhaji  Nikam <110546379+vrushalisambhajinikam@users.noreply.github.com>
2024-03-05 13:11:23 +0530

    Add files via upload

--- MinMaxAlgo_using_divide_and_conquer.cpp
+++ MinMaxAlgo_using_divide_and_conquer.cpp
@@ -1,66 +1,53 @@
- #include<iostream>
- using namespace std;
- 	
- 	int a[100];
- 	
- 	int minn,maxx;
- 	
- int MaxMinAlgo(int x, int y)
- {
- 	int max1,min1,mid;
- 	if(x==y)
- 	{
-	  	minn=maxx=a[x];
-	}
-	 if(x=y-1)
+#include<iostream> 
+using namespace std;
+
+int a[100];
+int minn, maxx;
+
+void MaxMinAlgo(int x, int y) {
+    int max1, min1, mid;
+    if (x == y)
+	 {
+        minn = maxx = a[x];
+    } 
+	else if (x == y - 1) 
 	{
-		if(a[x]<a[y])
+        if (a[x] < a[y])
+		 {
+            maxx = a[y];
+            minn = a[x];
+        } else 
 		{
-			maxx=a[y];
-			minn=a[x];
-		}
-		else
-		{
-			maxx=a[x];
-			minn=a[y];
-		}
-	}
-	else
-	{
-		mid=(x+y)/2;
-		
-		MaxMinAlgo(x,mid);
-		minn=min1;
-		maxx=max1;
-	
-		MaxMinAlgo(mid+1,y);
-		
-		if(maxx<max1)
-		{
-			maxx=max1;
-		}
-		if(minn>min1)
-		{
-			minn=min1;
-		}
-	}
-	 
- }
- int main()
- {
-	int n,i;
- 	cout<<"Enter array size:"<<endl;
- 	cin>>n; 
- 	 
- 	for(i=1;i<=n;i++)
- 	{
- 		cin>>a[i];
-	}
- 
-	minn=a[0];
-	maxx=a[0];
- 	 MaxMinAlgo(1,n);
- 	cout<<"Minimum element in array is: "<<minn<<endl;
- 	 cout<<"Maximum element in array is: "<<maxx<<endl;
- 	 return 0;
- }
+            maxx = a[x];
+            minn = a[y];
+        }
+    } else
+	 {
+        mid = (x + y) / 2;
+        MaxMinAlgo(x, mid);
+        min1 = minn;
+        max1 = maxx;
+        MaxMinAlgo(mid + 1, y);
+        if (maxx < max1) {
+            maxx = max1;
+        }
+        if (minn > min1) {
+            minn = min1;
+        }
+    }
+}
+
+int main() {
+    int n, i;
+    cout << "Enter array size:" << endl;
+    cin >> n;
+    for (i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+    minn = a[1];
+    maxx = a[1];
+    MaxMinAlgo(1, n);
+    cout << "Minimum element in array is: " << minn << endl;
+    cout << "Maximum element in array is: " << maxx << endl;
+    return 0;
+}
